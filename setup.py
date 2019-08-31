56bb1d6fa2f94f07ad7cc5c14fc301373ea50f82
Mauricio Moreira <phydev@users.noreply.github.com>
2019-08-31 02:12:58 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""
+this setup will check for dependencies and install PhyBio on your computer
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name = "regrets-collector",
+    version = "1.0.0",
+    url = "https://gitlab.com/phydev/pybot-exec.git",
+    author = "duckDev",
+    author_email = "mmsoares@uc.pt",
+    description = "pythonic bot",
+    license = "GNU GPLv3",
+    platform = "Python 3.7.2",
+    packages = find_packages(),
+    install_requires = ["tweepy >= 3.7.0"],
+)
