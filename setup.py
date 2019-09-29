b532ea8ff57c7f183ab9f656d89c20157d7d5d78
Mauricio Moreira <phydev@users.noreply.github.com>
2019-09-29 22:27:21 +0100

    update

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ this setup will check for dependencies and install PhyBio on your computer
 from setuptools import setup, find_packages
 
 setup(
-    name = "regrets-collector",
+    name = "pybot-exec",
     version = "1.0.0",
     url = "https://gitlab.com/phydev/pybot-exec.git",
     author = "duckDev",
     author_email = "mmsoares@uc.pt",
     description = "pythonic bot",
     license = "GNU GPLv3",
-    platform = "Python 3.7.2",
+    platform = "Python 3.7.4",
     packages = find_packages(),
-    install_requires = ["tweepy >= 3.7.0", "numpy >= 1.16.2", "matplotlib >= 3.0.3"],
+    install_requires = ["tweepy >= 3.8.0", "numpy >= 1.16.2", "matplotlib >= 3.0.3"],
 )
